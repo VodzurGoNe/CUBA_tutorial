a53ab9020dab4c2584383d12aa3bd8b94c98fad8
Vladislav Gruzdov <Gruzdov.V.1991@Hotmail.com>
2021-06-22 22:36:47 +0400

    I repeat the application on the training video.

---
+++ SessionPlanner/modules/global/src/com/company/planner/entity/Speaker.java
@@ -0,0 +1,51 @@
+package com.company.planner.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "PLANNER_SPEAKER")
+@Entity(name = "planner_Speaker")
+@NamePattern("%s %s|firstName,lastName")
+public class Speaker extends StandardEntity {
+    private static final long serialVersionUID = -1L;
+
+    @NotNull
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @NotNull
+    @Column(name = "EMAIL", nullable = false, unique = true)
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(java.lang.String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
